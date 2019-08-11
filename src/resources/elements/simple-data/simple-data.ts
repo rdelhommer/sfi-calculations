b381a5a89d9ff25db389b9c3b50501d1a4076b92
Rick Delhommer <rdelhommer@gmail.com>
2019-08-10 21:08:41 -0700

    basic print sheet

--- src/resources/elements/simple-data/simple-data.ts
+++ src/resources/elements/simple-data/simple-data.ts
@@ -1,10 +1,16 @@
 import { IOrganismData } from "../../../models/organism.model";
-import { bindable } from "aurelia-templating";
+import { bindable, containerless } from "aurelia-templating";
 import { IProfileModel } from "../../../models/profile.model";
 import { ISampleInfoModel } from "../../../models/sample.model";
+import './simple-data.scss'
 
+@containerless
 export class SimpleData {
   @bindable data: IOrganismData
   @bindable profile: IProfileModel
   @bindable sample: ISampleInfoModel
+
+  bind() {
+    console.log(this.data);
+  }
 }
