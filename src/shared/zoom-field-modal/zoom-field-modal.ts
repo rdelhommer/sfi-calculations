6b04ebb19e0366c16ba217ad5e3529d559f52ba8
Rick Delhommer <rdelhommer@wsecu.org>
2019-08-18 20:44:33 -0700

    add hello world zoom field dialog

--- src/shared/zoom-field-modal/zoom-field-modal.ts
+++ src/shared/zoom-field-modal/zoom-field-modal.ts
@@ -1,3 +1,17 @@
+import { DialogController } from "aurelia-dialog";
+import { inject } from "aurelia-framework";
+
+export interface IZoomFieldModalModel {
+
+}
+
+@inject(DialogController)
 export class ZoomFieldModal {
+  constructor(
+    private dialogController: DialogController
+  ) { }
   
+  activate(model: IZoomFieldModalModel) {
+
+  }
 }
