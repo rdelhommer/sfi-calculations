db817f2673c3310d19679808637f469794b6732e
Rick Delhommer <rdelhommer@wsecu.org>
2019-08-19 14:32:01 -0700

    add basic fungi reading/field ui

--- src/proto/fila-proto/fila-proto.ts
+++ src/proto/fila-proto/fila-proto.ts
@@ -2,10 +2,16 @@ import './fila-proto.scss'
 import { DialogService } from 'aurelia-dialog'
 import { inject } from 'aurelia-framework';
 import { ZoomFieldModal } from '../../shared/zoom-field-modal/zoom-field-modal';
+import { FungalColor, OomyceteColor } from '../../util/enums';
 
 @inject(DialogService)
 export class FilaProto {
   actinobacteriaReadings: any = [{}, {}, {}, {}, {}]
+  fungiReadings: any = [{}, {}, {}, {}, {}]
+  oomyceteReadings: any = [{}, {}, {}, {}, {}]
+
+  FungalColor = FungalColor
+  OomyceteColor = OomyceteColor
 
   constructor(
     private dialogService: DialogService
@@ -19,16 +25,48 @@ export class FilaProto {
         }
       });
     })
+
+    this.fungiReadings.forEach(x => {
+      x.fields = new Array(5).map(f => {
+        return {
+          length: null,
+          diameter: null,
+          color: null
+        }
+      });
+    })
+
+    this.oomyceteReadings.forEach(x => {
+      x.fields = new Array(5).map(f => {
+        return {
+          length: null,
+          diameter: null,
+          color: null
+        }
+      });
+    })
   }
 
   addField() {
     this.actinobacteriaReadings.forEach(x => x.fields.push({ length : null }))
+    this.fungiReadings.forEach(x => x.fields.push({
+      length: null,
+      diameter: null,
+      color: null
+    }))
+    this.oomyceteReadings.forEach(x => x.fields.push({
+      length: null,
+      diameter: null,
+      color: null
+    }))
   }
 
   removeField() {
     if (this.actinobacteriaReadings[0].fields.length <= 5) return
 
     this.actinobacteriaReadings.forEach(x => x.fields.pop())
+    this.fungiReadings.forEach(x => x.fields.pop())
+    this.oomyceteReadings.forEach(x => x.fields.pop())
   }
 
   collectData() {
