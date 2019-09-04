4fde4c54c7e11eec3a695e341900ab73a3bf1c18
Rick Delhommer <rdelhommer@wsecu.org>
2019-09-04 10:59:01 -0700

    hook up biomass UI

--- src/resources/elements/biomass/biomass.ts
+++ src/resources/elements/biomass/biomass.ts
@@ -1,5 +1,7 @@
 import './biomass.scss'
+import { bindable } from 'aurelia-framework';
+import { IOrganism } from '../../../models/organism/organism.model';
 
 export class Biomass {
-  
+  @bindable() organism: IOrganism<any>
 }
